09c2f2a1ea0e0d591200cd19cea90075b1813f27
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-06-10 21:05:21 +0800

    初始化 Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/',
+      name: 'web',
+      // component: () => import('@/views/web/index.vue')
+      redirect: '/admin'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login/index.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('@/views/admin/index.vue')
+    },
   ]
 })
 
