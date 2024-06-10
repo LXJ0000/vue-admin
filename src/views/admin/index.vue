27b9e60d9d3a2ac54975281bc39807bcc577a754
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-06-10 21:20:18 +0800

    Admin 基本结构

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>Admin</div>
+  <div class="admin">
+    <div class="aside">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="main">
+      <div class="header">
+        <div class="bread_crumb"></div>
+        <div class="action">
+          <icon-home />
+          <icon-sun-fill />
+          <icon-moon-fill />
+          <icon-fullscreen-exit />
+          <icon-fullscreen />
+          <div class="user"></div>
+        </div>
+      </div>
+      <div class="tab"></div>
+      <div class="content">
+        <router-view></router-view>
+      </div>
+      <div class="footer"></div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped lang="less"></style>
