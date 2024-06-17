4248b2449cbabcaf6e980cda2c7ea32f3a32dc75
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-06-17 23:15:10 +0800

    feat: Admin 样式2

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -1,79 +1,209 @@
 <template>
-  <div class="admin">
-    <div class="aside">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
-    <div class="main">
-      <div class="header">
-        <div class="bread_crumb">bread_crumb</div>
+  <a-layout class="layout-demo">
+    <a-layout-sider
+      theme="dark"
+      breakpoint="lg"
+      :width="220"
+      collapsible
+      :collapsed="collapsed"
+      @collapse="onCollapse"
+    >
+      <div class="logo" />
+      <a-menu
+        :defaultOpenKeys="['1']"
+        :defaultSelectedKeys="['0_2']"
+        @menuItemClick="onClickMenuItem"
+      >
+        <a-menu-item key="0_1" disabled>
+          <IconHome />
+          Menu 1
+        </a-menu-item>
+        <a-menu-item key="0_2">
+          <IconCalendar />
+          Menu 2
+        </a-menu-item>
+        <a-sub-menu key="1">
+          <template #title>
+            <span><IconCalendar />Navigation 1</span>
+          </template>
+          <a-menu-item key="1_1">Menu 1</a-menu-item>
+          <a-menu-item key="1_2">Menu 2</a-menu-item>
+          <a-sub-menu key="2" title="Navigation 2">
+            <a-menu-item key="2_1">Menu 1</a-menu-item>
+            <a-menu-item key="2_2">Menu 2</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="3" title="Navigation 3">
+            <a-menu-item key="3_1">Menu 1</a-menu-item>
+            <a-menu-item key="3_2">Menu 2</a-menu-item>
+            <a-menu-item key="3_3">Menu 3</a-menu-item>
+          </a-sub-menu>
+        </a-sub-menu>
+        <a-sub-menu key="4">
+          <template #title>
+            <span><IconCalendar />Navigation 4</span>
+          </template>
+          <a-menu-item key="4_1">Menu 1</a-menu-item>
+          <a-menu-item key="4_2">Menu 2</a-menu-item>
+          <a-menu-item key="4_3">Menu 3</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header>
+        <a-menu :openKeys="['1']" :selectedKeys="['0_2']" mode="horizontal">
+          <a-menu-item key="0_1" disabled>
+            <IconHome />
+            Menu 1
+          </a-menu-item>
+          <a-menu-item key="0_2">
+            <IconCalendar />
+            Menu 2
+          </a-menu-item>
+          <a-sub-menu key="1">
+            <template #title>
+              <span><IconCalendar />Navigation 1</span>
+            </template>
+            <a-menu-item key="1_1">Menu 1</a-menu-item>
+            <a-menu-item key="1_2">Menu 2</a-menu-item>
+            <a-sub-menu key="2" title="Navigation 2">
+              <a-menu-item key="2_1">Menu 1</a-menu-item>
+              <a-menu-item key="2_2">Menu 2</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="3" title="Navigation 3">
+              <a-menu-item key="3_1">Menu 1</a-menu-item>
+              <a-menu-item key="3_2">Menu 2</a-menu-item>
+              <a-menu-item key="3_3">Menu 3</a-menu-item>
+            </a-sub-menu>
+          </a-sub-menu>
+          <a-sub-menu key="4">
+            <template #title>
+              <span><IconCalendar />Navigation 4</span>
+            </template>
+            <a-menu-item key="4_1">Menu 1</a-menu-item>
+            <a-menu-item key="4_2">Menu 2</a-menu-item>
+            <a-menu-item key="4_3">Menu 3</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
         <div class="action">
           <icon-home />
           <icon-sun-fill />
           <icon-moon-fill />
           <icon-fullscreen-exit />
           <icon-fullscreen />
-          <div class="user">user</div>
+          <icon-notification />
+          <div class="user">
+            <a-dropdown>
+              <a-avatar :style="{ backgroundColor: '#00d0b6' }":size="40">
+                Marcus
+              </a-avatar>
+              <template #content>
+                <a-doption>个人中心</a-doption>
+                <a-doption :value="{ value: 'Setting' }">设置</a-doption>
+                <a-doption disabled>退出登录</a-doption>
+              </template>
+            </a-dropdown>
+          </div>
         </div>
-      </div>
-      <div class="tab"></div>
-      <div class="content">
-        <router-view></router-view>
-      </div>
-    </div>
-  </div>
+      </a-layout-header>
+      <a-layout style="padding: 0 24px">
+        <a-breadcrumb :style="{ margin: '16px 0' }">
+          <a-breadcrumb-item>首页</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content>
+          Content
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer>Footer</a-layout-footer>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
+<script>
+import { defineComponent, ref } from 'vue'
+import { Message } from '@arco-design/web-vue'
+import { IconHome, IconCalendar } from '@arco-design/web-vue/es/icon'
 
-<script setup></script>
-
-<style lang="less">
-.admin {
-  display: flex;
+export default defineComponent({
+  components: {
+    IconHome,
+    IconCalendar,
+  },
+  setup() {
+    const collapsed = ref(false)
+    const onCollapse = (val, type) => {
+      const content = type === 'responsive' ? '触发响应式收缩' : '点击触发收缩'
+      Message.info({
+        content,
+        duration: 2000,
+      })
+      collapsed.value = val
+    }
+    return {
+      collapsed,
+      onCollapse,
+      onClickMenuItem(key) {
+        Message.info({ content: `You select ${key}`, showIcon: true })
+      },
+    }
+  },
+})
+</script>
+<style scoped>
+.layout-demo {
   height: 100vh;
-  .aside {
-    width: 200px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    border-right: 1px solid var(--color-neutral-2);
-    .logo {
-      height: 100px;
-      width: 100%;
-      border-bottom: 1px solid var(--color-neutral-2);
-    }
+  background: var(--color-fill-2);
+}
+.layout-demo :deep(.arco-layout-sider) .logo {
+  height: 32px;
+  margin: 12px 8px;
+  background: rgba(255, 255, 255, 0.2);
+}
+.layout-demo :deep(.arco-layout-sider-light) .logo {
+  background: var(--color-fill-2);
+}
+.layout-demo :deep(.arco-layout-header) {
+  height: 64px;
+  line-height: 64px;
+  background: var(--color-bg-3);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .action {
+    margin-right: 24px;
+    display: flex;
+    align-items: center;
   }
-  .main {
-    width: calc(100% - 200px);
-    .header {
-      height: 60px;
-      width: 100%;
-      border-bottom: 1px solid var(--color-neutral-2);
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      .bread_crumb {
-        margin-left: 10px; /* Space from the left */
-      }
-      .action {
-        margin-right: 10px;
-        display: flex;
-        align-items: center;
-      }
-      .action > *:not(:last-child) {
-        margin-right: 10px; /* Space between elements inside action */
-        font-size: 16px;
-      }
-    }
-    .tab {
-      height: 40px;
-      width: 100%;
-      border-bottom: 1px solid var(--color-neutral-2);
-    }
-    .content {
-      height: calc(100% - 100px - 2px);
-      width: 100%;
-      overflow-y: auto;
-      overflow-x: hidden;
-    }
+  .action > *:not(:last-child) {
+    margin-right: 24px;
+    font-size: 20px;
   }
+  .action:hover {
+    cursor: pointer;
+  }
+}
+.layout-demo :deep(.arco-layout-footer) {
+  height: 48px;
+  color: var(--color-text-2);
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 48px;
+}
+.layout-demo :deep(.arco-layout-content) {
+  color: var(--color-text-2);
+  font-weight: 400;
+  font-size: 14px;
+  background: var(--color-bg-3);
+}
+.layout-demo :deep(.arco-layout-footer),
+.layout-demo :deep(.arco-layout-content) {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: var(--color-white);
+  font-size: 16px;
+  font-stretch: condensed;
+  text-align: center;
 }
 </style>
